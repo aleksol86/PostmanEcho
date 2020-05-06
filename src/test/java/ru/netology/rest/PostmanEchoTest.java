933cc36db3168d4668a1023a9bbb4f274aa5a279
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-05-06 20:09:15 +0300

    Добавлен .appveyor.yml

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldTest1() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("postman.schema.json"));
+    }
+}
